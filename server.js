8d9268fcbd24f3ff12668dd7a7d55659481b8c4e
Santhosh Kumar G <skclouduser@gmail.com>
2022-03-17 00:42:16 +0530

    Angular deployment config changes

--- server.js
+++ server.js
@@ -10,12 +10,13 @@ const app = express();
 
 // app.use(requireHTTPS);
 
-app.use(express.static('./dist/hdls-admin-ui'));
+// app.use(express.static('./dist/hdls-admin-ui'));
+app.use(express.static(__dirname + '/dist/hdls-admin-ui'));
 
-app.get('*', function (req, res) {
-    const index = path.join(__dirname, 'build', 'index.html');
-    res.sendFile(index);
-  });
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname + '/dist/hdls-admin-ui/index.html'));
+});
+
 app.listen(process.env.PORT || 8080,()=>{
     console.log("Server listening on Port",process.env.PORT || 8080);
 });
