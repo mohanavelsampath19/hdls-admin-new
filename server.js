e95df57f5d18cc7d2e0dd3bd65a93ca44f818dea
Santhosh Kumar G <skclouduser@gmail.com>
2022-03-17 01:11:24 +0530

    Deployment fix

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/hdls-admin-ui'));
+app.use(express.static('./dist/hdls-admin'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/hdls-admin-ui/'}),
+app.get('*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/hdls-admin/'}),
 );
 
 // Start the app by listening on the default Heroku port
