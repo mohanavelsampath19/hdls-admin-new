d6f5018ad73ece235f8dbded5be94fdfc2474f71
mohanavelsampath19 <mohanavelsampath@gmail.com>
2025-07-12 15:32:12 +0530

    Booking detail related changes are updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
- // baseUrl: 'https://cloud.tribeloyalty.my/',
-  baseUrl: 'http://localhost/',
+  baseUrl: 'https://cloud.tribeloyalty.my/',
   imageUrl: 'https://cloud.tribeloyalty.my/upload',
   socketUrl: '',
   // imageUrl: 'http://localhost:5000/upload'
